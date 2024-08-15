ddf5504bd2b21127e89649aed7d4d020c1b4be4a
funa <orz.home35@gmail.com>
2024-08-15 18:17:05 +0900

    Renamed file, added ATD.

---
+++ ADT_240814/b_longestpalindrome.rb
@@ -0,0 +1,17 @@
+def main
+	s = gets.chomp
+
+	max_len = 1
+
+	(0...s.length).each do |i|
+	  (i...s.length).each do |j|
+		substring = s[i..j]
+		if substring == substring.reverse
+		  max_len = [max_len, substring.length].max
+		end
+	  end
+	end
+
+	puts max_len
+  end
+  main
